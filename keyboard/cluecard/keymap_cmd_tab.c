1d19821688c0d0173bc7105b9e4a8b96cb9adf37
skullY <skullydazed@gmail.com>
2015-09-09 07:53:54 -0700

    Add some experimental macro code

--- keyboard/cluecard/keymap_cmd_tab.c
+++ keyboard/cluecard/keymap_cmd_tab.c
@@ -6,9 +6,28 @@ const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * |FN0|
      * `---`
      */
-    KEYMAP(FN0),
+    KEYMAP(FN1)
+};
+
+enum macro_id {
+    CMD_TAB,
 };
 
 const uint16_t PROGMEM fn_actions[] = {
-    [0] = ACTION_MODS_KEY(MOD_LGUI, KC_TAB)
+    [0] = ACTION_MACRO(CMD_TAB),
+    [1] = ACTION_MODS_KEY(MOD_LGUI, KC_TAB)
 };
+
+/*
+ * Macro definition
+ */
+const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt)
+{
+    switch (id) {
+        case CMD_TAB:
+            return (record->event.pressed ?
+                    MACRO(D(LGUI), T(TAB), U(LGUI), END) :
+                    MACRO(END));
+    }
+    return MACRO_NONE;
+}

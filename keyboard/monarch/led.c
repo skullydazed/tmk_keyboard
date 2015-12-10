517fd6b064d5b5a1efce0f0a12a3fdc085d42370
skullY <skullydazed@gmail.com>
2015-12-10 01:17:20 -0800

    Add capslock/numlock support to the monarch

--- keyboard/monarch/led.c
+++ keyboard/monarch/led.c
@@ -18,26 +18,27 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include <avr/io.h>
 #include "stdint.h"
 #include "led.h"
+#include "print.h"
 
 
 void led_set(uint8_t usb_led)
 {
     if (usb_led & (1<<USB_LED_CAPS_LOCK)) {
-        // output low
-        DDRB    |= (1<<6);
-        PORTB &= ~(1<<6);
+        // output high
+        DDRB  |= (1<<6);
+        PORTB |= (1<<6);
     } else {
-        // Hi-Z
-        DDRB &= ~(1<<6);
+        // output low
+        DDRB  &= ~(1<<6);
         PORTB &= ~(1<<6);
     }
 
     if (usb_led & (1<<USB_LED_NUM_LOCK)) {
-        // output low
+        // output high
         DDRC |= (1<<6);
-        PORTC &= ~(1<<6);
+        PORTC |= (1<<6);
     } else {
-        // Hi-Z
+        // output low
         DDRC &= ~(1<<6);
         PORTC &= ~(1<<6);
     }
